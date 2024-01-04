aac5869b7a7ebb3a9122734c975aee33596f6f6c
Alacant2804 <alacant2804@gmail.com>
2024-01-03 21:09:34 -0500

    Updated functions for players object

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+const myName = function(_default = 'Player') {
+    const playerName = prompt("Player's name: ") || _default;
+    return playerName;
+}
+
+const mySymbol = function() {
+    let playerSymbol;
+    do {
+        playerSymbol = prompt("Player's symbol (X or O): ");
+        if (playerSymbol !== 'X' && playerSymbol !== 'O') {
+            alert('Invalid symbol, try again!');
+        }
+    } while (playerSymbol !== 'X' && playerSymbol !== 'O');
+
+    return playerSymbol;
+}
+
+
 const gameboard = {
 
 }
 
 const players = {
-    name: myName(),
+    name: myName,
     symbol: mySymbol
 }
 
@@ -11,15 +29,3 @@ const game = {
 
 }
 
-const myName = function(_default = 'Player') {
-    const playerName = prompt("Player's name: ") || _default;
-    return playerName;
-}
-
-const mySymbol = function() {
-    const playerSymbol = prompt("Player's symbol: ");
-    if (playerSymbol != 'X' && playerSymbol != 'O') {
-        alert('Invalid symbol, try again!');
-    }
-    return playerSymbol;
-}
